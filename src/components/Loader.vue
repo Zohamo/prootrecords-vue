52b02d91ae1436006cf980bf8237a8bad0ca5cf1
Zohamo <thiazsch@gmail.com>
2021-05-09 17:03:31 +0200

    feat(loader) init

---
+++ src/components/Loader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="d-flex justify-center align-center"
+    style="width: 100%; height: 100%"
+  >
+    <v-progress-circular :size="100" :width="15" color="grey" indeterminate />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Loader",
+});
+</script>
