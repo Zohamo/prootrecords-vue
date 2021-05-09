af3b7632de9ee950a78b243c95f7ad15db62afa8
Zohamo <thiazsch@gmail.com>
2021-05-09 14:07:49 +0200

    feat(about) init content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1 class="text-h2 mb-16">About Proot records</h1>
+
+    <p>
+      <strong>Proot Records</strong> is a Netlabel founded by
+      <a href="https://thiazitch.com/">Thiaz Itch</a> in 2006.
+    </p>
+
+    <p>
+      Its purpose is to gather artists making
+      <strong>happy and original music</strong>, by sharing
+      <strong>free albums</strong> to listen and/or download, and producing
+      physical releases.
+    </p>
+
+    <p>
+      By extension, Proot works also with graphical artists for the artworks of
+      its albums and some short films like movie clips and teasers.
+    </p>
+
+    <p>
+      Proot is based in Toulouse (France) but the artists that feature on the
+      label come from everywhere in the world.
+    </p>
   </div>
 </template>
