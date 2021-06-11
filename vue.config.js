18c5f16dba637f08e4da90fb37a6abdd8dff61ea
Zohamo <thiazsch@gmail.com>
2021-06-11 21:46:49 +0200

    feat(meta) edit title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Proot records &#9729; frivolous and original music'
+        return args
+      })
+  }
 };
