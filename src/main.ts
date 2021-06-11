4c60621365b4c8ad096cdb79e62cefbcd4acc74b
Zohamo <thiazsch@gmail.com>
2021-06-11 22:45:43 +0200

    config(store) implement vuex

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./plugins/store";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
@@ -11,6 +12,7 @@ Vue.prototype.$bandcampLinkColor = "7B1FA2";
 
 new Vue({
   router,
+  store,
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
