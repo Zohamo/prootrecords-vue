9d8b3a555f5e6b183811d89cc4d0f09451137b6f
Zohamo <thiazsch@gmail.com>
2021-07-01 13:28:55 +0200

    refactor(home) extract prooject list item

---
+++ src/components/HomeProojectListItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-card-title>
+      <div class="d-flex justify-space-between" style="width: 100%">
+        <h3>Prooject {{ prooject.title }}</h3>
+
+        <v-btn plain :to="'/proojects/' + prooject.slug">
+          <v-icon v-text="'mdi-arrow-right'" />
+        </v-btn>
+      </div>
+    </v-card-title>
+
+    <v-list dense>
+      <v-list-item
+        two-line
+        v-for="track of prooject.tracks.slice(0, 3)"
+        :key="track.position"
+      >
+        <v-list-item-content>
+          <v-list-item-title>
+            <span v-html="track.artist" class="grey--text mr-2" />
+            <span v-html="track.title" />
+          </v-list-item-title>
+
+          <v-list-item-subtitle
+            v-if="track.info"
+            class="d-inline-block text-truncate"
+            v-html="track.info.content"
+          />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prooject } from "@/types";
+import { PropType } from "vue";
+export default {
+  name: "ProojectHomeListItem",
+
+  props: {
+    prooject: {
+      type: Object as PropType<Prooject>,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,46 +4,22 @@
       <v-row>
         <v-col v-if="release" cols="9" style="max-width: 1000px">
           <div class="d-flex flex-column">
-            <v-subheader class="primary--text">Last Release</v-subheader>
+            <v-subheader class="primary--text" v-html="'Last Release'" />
             <last-release :release="release" />
           </div>
         </v-col>
         <v-col>
           <div class="d-flex flex-column">
-            <v-subheader class="primary--text"
-              >Prootjects &middot; Last Entries</v-subheader
-            >
+            <v-subheader
+              class="primary--text"
+              v-html="'Prootjects &middot; Last Entries'"
+            />
             <v-card>
-              <div v-for="prooject of proojects" :key="prooject.id">
-                <v-card-title>
-                  <div class="d-flex justify-space-between" style="width: 100%">
-                    <h3>Prooject {{ prooject.title }}</h3>
-                    <v-btn plain :to="'/proojects/' + prooject.slug">
-                      <v-icon v-text="'mdi-arrow-right'" />
-                    </v-btn>
-                  </div>
-                </v-card-title>
-
-                <v-list dense>
-                  <v-list-item
-                    two-line
-                    v-for="track of prooject.tracks"
-                    :key="track.position"
-                  >
-                    <v-list-item-content>
-                      <v-list-item-title>
-                        <span v-html="track.artist" class="grey--text mr-2" />
-                        <span v-html="track.title" />
-                      </v-list-item-title>
-                      <v-list-item-subtitle
-                        v-if="track.info"
-                        class="d-inline-block text-truncate"
-                        v-html="track.info.content"
-                      />
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </div>
+              <home-prooject-list-item
+                v-for="prooject of proojects"
+                :key="prooject.id"
+                :prooject="prooject"
+              />
             </v-card>
           </div>
         </v-col>
@@ -54,11 +30,12 @@
 
 <script lang="ts">
 import LastRelease from "@/components/LastRelease.vue";
+import HomeProojectListItem from "@/components/HomeProojectListItem.vue";
 import { mapActions, mapState } from "vuex";
 export default {
   name: "Home",
 
-  components: { LastRelease },
+  components: { LastRelease, HomeProojectListItem },
 
   computed: {
     ...mapState(["release", "releases", "proojects"]),
